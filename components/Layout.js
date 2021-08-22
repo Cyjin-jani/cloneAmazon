378a44a2834c5ebe351bcf904624c1d2cec1a541
yjcho <jinjanic91@gmail.com>
2021-08-22 17:06:31 +0900

    first commit

---
+++ components/Layout.js
@@ -0,0 +1,47 @@
+import {
+  AppBar,
+  Container,
+  Link,
+  Toolbar,
+  Typography,
+} from '@material-ui/core';
+import Head from 'next/head';
+import NextLink from 'next/link';
+import React from 'react';
+import useStyles from '../utils/styles';
+
+export default function Layout({ title, description, children }) {
+  const classes = useStyles();
+  return (
+    <div>
+      <Head>
+        <title>
+          {title ? `${title} - Next AmazonaClone` : 'Next AmazonClone'}
+        </title>
+        {description && <meta name="description" content={description}></meta>}
+      </Head>
+      <AppBar position="static" className={classes.navbar}>
+        <Toolbar>
+          <NextLink href="/" passHref>
+            <Link>
+              <Typography className={classes.brand}>Amazona</Typography>
+            </Link>
+          </NextLink>
+          <div className={classes.grow}></div>
+          <div>
+            <NextLink href="/cart" passHref>
+              <Link>Cart</Link>
+            </NextLink>
+            <NextLink href="/login" passHref>
+              <Link>Login</Link>
+            </NextLink>
+          </div>
+        </Toolbar>
+      </AppBar>
+      <Container className={classes.main}>{children}</Container>
+      <footer className={classes.footer}>
+        <Typography>All rights reserved. Next Amazona</Typography>
+      </footer>
+    </div>
+  );
+}
