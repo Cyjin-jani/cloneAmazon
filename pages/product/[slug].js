17ddeebef2e9a5fe63b9754cb975cfb84d52aab1
yjcho <jinjanic91@gmail.com>
2021-08-31 21:44:23 +0900

    add to cart

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -7,22 +7,33 @@ import {
   ListItem,
   Typography,
 } from '@material-ui/core';
-
+import axios from 'axios';
 import NextLink from 'next/link';
 import Image from 'next/image';
-import React from 'react';
+import React, { useContext } from 'react';
 import Layout from '../../components/Layout';
 import useStyles from '../../utils/styles';
 import Product from '../../models/Product';
 import db from '../../utils/db';
+import { Store } from '../../utils/Store';
 
 export default function ProductScreen(props) {
+  const { dispatch } = useContext(Store);
   const { product } = props;
   const classes = useStyles();
 
   if (!product) {
     return <div>Product Not Found</div>;
   }
+
+  const addToCartHandler = async () => {
+    const { data } = await axios.get(`/api/products/${product._id}`);
+    if (data.countInStock <= 0) {
+      window.alert('sorry. Product is out of stock');
+      return;
+    }
+    dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity: 1 } });
+  };
 
   return (
     <Layout title={product.name} description={product.description}>
@@ -92,7 +103,12 @@ export default function ProductScreen(props) {
                 </Grid>
               </ListItem>
               <ListItem>
-                <Button fullWidth variant="contained" color="primary">
+                <Button
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  onClick={addToCartHandler}
+                >
                   Add to cart
                 </Button>
               </ListItem>
